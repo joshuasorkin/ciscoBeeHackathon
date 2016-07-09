2bcc05bf50d44d5479942ab161466485f2a7b6fb
joshua sorkin <joshua.d.sorkin@gmail.com>
2016-07-09 13:27:02 -0700

    added voice interaction

--- notifier.js
+++ notifier.js
@@ -1,3 +1,19 @@
+/*
 call("+15105753138", {
    network:"SMS"});
 say("Your beehive is in danger!");
+*/
+
+result = ask("Your beehive is under attack!  Will you help or run away?", {
+   choices:"help, run away"
+});
+
+if (result.value=="help"){
+	say("You saved the bees!");
+}
+else if (result.value=="run away"){
+	say("The bees have been kidnapped!");
+}
+else {
+	say("I did not understand your command.");
+}
