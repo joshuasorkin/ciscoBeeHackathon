30cd0c720d1fec8a037bb3dc4e2b37e88eb566ab
joshua sorkin <joshua.d.sorkin@gmail.com>
2016-07-09 17:59:55 -0700

    changed notifier to use mode parameter

--- notifier.js
+++ notifier.js
@@ -1,21 +1,33 @@
-call("+15105753138", {
-   network:"SMS"}
-   callerID:'18005555338');
-say("Your beehive is in danger! Visit hyperspacecraft.net/beeApp/map.html for details.");
-
-
-/*
-result = ask("Your beehive is under attack!  Will you help or run away?", {
-   choices:"help, run away"
-});
-
-if (result.value=="help"){
-	say("You saved the bees!");
+var modeLower=mode.toLowerCase();
+switch (mode)
+{
+	case 'sms':
+		smsNotify();
+		break;
+	case 'voice':
+		voiceNotify();
+		break;
 }
-else if (result.value=="run away"){
-	say("The bees have been kidnapped!");
+
+function smsNotify(){
+	call("+15105753138", {
+	   network:"SMS"}
+	   callerID:'18005555338');
+	say("Your beehive is in danger! Visit hyperspacecraft.net/beeApp/map.html for details.");
 }
-else {
-	say("I did not understand your command.");
+
+function voiceNotify(){
+	result = ask("Your beehive is under attack!  Will you help or run away?", {
+	   choices:"help, run away"
+	});
+
+	if (result.value=="help"){
+		say("You saved the bees!");
+	}
+	else if (result.value=="run away"){
+		say("The bees have been kidnapped!");
+	}
+	else {
+		say("I did not understand your command.");
+	}
 }
-*/
